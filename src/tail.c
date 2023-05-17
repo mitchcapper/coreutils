7a0522c0657b52a5f83bcc49ec4357f64515ee1c
Mitch Capper <mitch.capper@gmail.com>
2023-05-17 13:17:04 -0700

    Windows compatibility improvements

--- src/tail.c
+++ src/tail.c
@@ -346,7 +346,9 @@ named file in a way that accommodates renaming, removal and creation.\n\
 static void
 die_pipe (void)
 {
+#ifndef _WIN32  
   raise (SIGPIPE);
+#endif
   exit (EXIT_FAILURE);
 }
 
